82dd12146d0aad0a438dc73c11c82bf1ebb01dfe
elmanzeynalov <elman.zeynalov@gmail.com>
2025-01-04 17:00:16 +0400

    Add responsive navbar with mobile support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-'use client';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-import { Button } from '@/components/ui/button';
-import ModeToggle from '@/components/ModeToggle';
-
 export default function Home() {
-	return (
-		<div>
-			<SignedOut>
-				<SignInButton mode={'modal'}>
-					<Button>sing in</Button>
-				</SignInButton>
-			</SignedOut>
-			<SignedIn>
-				<UserButton />
-			</SignedIn>
-			<ModeToggle />
-		</div>
-	);
+	return <div>home page</div>;
 }
